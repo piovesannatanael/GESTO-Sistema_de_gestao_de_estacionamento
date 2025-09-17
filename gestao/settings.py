b5453812fa4797c16a20828b68934f8cf2143f2c
Natanael <piovesannatanael@gmail.com>
2025-09-17 19:40:46 -0300

    App e CRUD de funcionarios

--- gestao/settings.py
+++ gestao/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += ['django_bootstrap5','stdimage']
-INSTALLED_APPS += ['home','clientes']
+INSTALLED_APPS += ['home','clientes','funcionarios']
 
 
 MIDDLEWARE = [
