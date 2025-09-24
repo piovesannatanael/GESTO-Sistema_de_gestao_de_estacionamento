6458a5473da49f276613ece102f9c57a09f21592
Natanael <piovesannatanael@gmail.com>
2025-09-23 23:26:59 -0300

    Antes de refazer as migrations

--- veiculos/forms.py
+++ veiculos/forms.py
@@ -1,16 +1,47 @@
 from django import forms
+from django.contrib.contenttypes.models import ContentType
+
+from clientes.models import ClientePJ, ClientePF
 from veiculos.models import Veiculo
 
 
 class VeiculoModelForm(forms.ModelForm):
+    cliente = forms.ChoiceField(label='Cliente', required=True)
+
     class Meta:
         model = Veiculo
-        fields = '__all__'
+        fields = ['placa', 'marca', 'modelo', 'cor', 'qtd_rodas']
 
-        error_messages = {
-            'placa':{'required':'A placa do veiculo é um campo obrigatório',
-                     'unique':'Placa já cadastrada'},
-            'qtd_rodas':{'required':'Quantidade de rodas do veiculo é obrigatória'},
-            'clientepf':{'required':'É obrigatório o nome do cliente'},
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        }
+        # 2. ESTA PARTE BUSCA TODOS OS CLIENTES (PF e PJ)
+        # E OS COLOCA COMO OPÇÕES NO SELETOR
+        clientes_pf = ClientePF.objects.all()
+        clientes_pj = ClientePJ.objects.all()
+        choices = [('', '---------')]
+
+        for cliente in clientes_pf:
+            ct = ContentType.objects.get_for_model(cliente)
+            choices.append((f'{ct.id}-{cliente.id}', f'{cliente.nome} (PF)'))
+
+        for cliente in clientes_pj:
+            ct = ContentType.objects.get_for_model(cliente)
+            choices.append((f'{ct.id}-{cliente.id}', f'{cliente.nome} (PJ)'))
+
+        self.fields['cliente'].choices = choices
+
+        # Preenche o valor inicial se estiver editando
+        if self.instance and self.instance.pk and self.instance.cliente:
+            ct = ContentType.objects.get_for_model(self.instance.cliente)
+            self.fields['cliente'].initial = f'{ct.id}-{self.instance.cliente.id}'
+
+    def save(self, commit=True):
+        cliente_data = self.cleaned_data['cliente'].split('-')
+        content_type_id = int(cliente_data[0])
+        object_id = int(cliente_data[1])
+
+        self.instance.content_type_id = content_type_id
+        self.instance.object_id = object_id
+
+        return super().save(commit)
--- veiculos/models.py
+++ veiculos/models.py
@@ -1,5 +1,9 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db.models.functions import Upper
 from django.db import models
+
+import clientes.models
 
 
 class Veiculo(models.Model):
@@ -8,13 +12,18 @@ class Veiculo(models.Model):
     modelo = models.CharField('Modelo', max_length=15, help_text='Modelo do veiculo')
     cor = models.CharField('Cor', max_length=15, help_text='Cor do veiculo')
     qtd_rodas = models.DecimalField('Quantidade de rodas', max_digits=2, decimal_places=0, help_text='Quantidade de rodas do veículo')
-    '''cliente = models.ForeignKey(clientes.models.ClientePF, verbose_name='Cliente PF', on_delete=models.PROTECT(), help_text="Nome do cliente",
-                                related_name='clientepf')'''
-
+    '''cliente = models.ManyToManyField(clientes.models.ClientePF and clientes.models.ClientePJ, verbose_name='Cliente PF', help_text="Nome do cliente",
+                                related_name='cliente')'''
+    content_type = models.ForeignKey(ContentType, on_delete=models.PROTECT) # Usamos PROTECT aqui
+    object_id = models.PositiveIntegerField()
+    cliente = GenericForeignKey('content_type', 'object_id')
     class Meta:
         verbose_name = 'Veiculo'
         verbose_name_plural = 'Veiculos'
         ordering = [Upper('placa')]
 
     def __str__(self):
-        return self.nome
+        return self.placa
+
+
+
