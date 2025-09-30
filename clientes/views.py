c682ac81b1bea4efa87d904dec93f058c0ab2ef4
piovesannatanael <piovesannatanael@gmail.com>
2025-09-30 00:18:00 -0300

    Preparando templates
    # Conflicts:
    #	clientes/migrations/0001_initial.py
    #	clientes/views.py
    #	funcionarios/migrations/0001_initial.py
    #	home/templates/head.html
    #	home/templates/principal.html
    #	vagas/migrations/0001_initial.py
    #	veiculos/forms.py
    #	veiculos/migrations/0001_initial.py

--- clientes/views.py
+++ clientes/views.py
@@ -13,23 +13,23 @@ from clientes.models import ClientePF, ClientePJ
 
 ### CRUD Clientes Pessoa Fisica
 
-# class ClientesPFView(ListView):
-#     model = ClientePF
-#     template_name = 'clientes.html'
-#
-#     def get_queryset(self):
-#         buscar = self.request.GET.get('buscar')
-#         qs = super(ClientesPFView, self).get_queryset()
-#         if buscar:
-#             qs = qs.filter(nome__icontains=buscar)
-#
-#         if qs.count() > 0:
-#             paginator = Paginator(qs, 1)
-#             listagem = paginator.get_page(self.request.GET.get('page'))
-#             return listagem
-#         else:
-#             return messages.info(self.request, 'Nenhum cliente cadastrado!')
+'''class ClientesPFView(ListView):
+    model = ClientePF
+    template_name = 'clientes.html'
 
+    def get_queryset(self):
+        buscar = self.request.GET.get('buscar')
+        qs = super(ClientesPFView, self).get_queryset()
+        if buscar:
+            qs = qs.filter(nome__icontains=buscar)
+
+        if qs.count() > 0:
+            paginator = Paginator(qs, 1)
+            listagem = paginator.get_page(self.request.GET.get('page'))
+            return listagem
+        else:
+            return messages.info(self.request, 'Nenhum cliente cadastrado!')
+'''
 class ClientePFAddView(SuccessMessageMixin, CreateView):
     model = ClientePF
     form_class = ClientePFModelForm
@@ -52,13 +52,14 @@ class ClientePFDeleteView(SuccessMessageMixin, DeleteView):
 
 
 ### CRUD Clientes Pessoa Juridica
-class ClientesView(ListView):
-    model = ClientePF or ClientePJ
+
+'''class ClientesPJView(ListView):
+    model = ClientePJ
     template_name = 'clientes.html'
 
     def get_queryset(self):
         buscar = self.request.GET.get('buscar')
-        qs = super(ClientesView, self).get_queryset()
+        qs = super(ClientesPJView, self).get_queryset()
         if buscar:
             qs = qs.filter(nome__icontains=buscar)
 
@@ -67,26 +68,8 @@ class ClientesView(ListView):
             listagem = paginator.get_page(self.request.GET.get('page'))
             return listagem
         else:
-            return messages.info(self.request, 'Nenhum CLIENTE OU EMPRESA cadastrada!')
-
-
-# class ClientesPJView(ListView):
-#     model = ClientePJ
-#     template_name = 'clientes.html'
-#
-#     def get_queryset(self):
-#         buscar = self.request.GET.get('buscar')
-#         qs = super(ClientesPJView, self).get_queryset()
-#         if buscar:
-#             qs = qs.filter(nome__icontains=buscar)
-#
-#         if qs.count() > 0:
-#             paginator = Paginator(qs, 1)
-#             listagem = paginator.get_page(self.request.GET.get('page'))
-#             return listagem
-#         else:
-#             return messages.info(self.request, 'Nenhuma empresa cadastrada!')
-
+            return messages.info(self.request, 'Nenhuma empresa cadastrada!')
+'''
 class ClientePJAddView(SuccessMessageMixin, CreateView):
     model = ClientePJ
     form_class = ClientePJModelForm
@@ -112,3 +95,82 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 
 
+def ClientesView(request):
+    buscar = request.GET.get('buscar')
+
+    clientespf_qs = ClientePF.objects.all()
+    clientespj_qs = ClientePJ.objects.all()
+
+    if buscar:
+        clientespf_qs = clientespf_qs.filter(nome__icontains=buscar)
+        clientespj_qs = clientespj_qs.filter(nome__icontains=buscar)
+
+    lista_clientes = sorted(
+        chain(clientespf_qs, clientespj_qs),
+        key=lambda instance: instance.nome.upper()
+    )
+
+    paginator = Paginator(lista_clientes, 1)
+    page_num = request.GET.get('page')
+    page_obj = paginator.get_page(page_num)
+
+    context = {
+        'object_list': page_obj,
+        'buscar': buscar if buscar else '',
+    }
+    return render(request, 'clientes.html', context)
+
+
+
+
+
+'''def ClientesView(request):
+    clientespf_qs = ClientePF.objects.all().order_by('nome')
+    clientespj_qs = ClientePJ.objects.all().order_by('nome')
+
+    # Paginação (opcional) — 10 por página para cada lista
+    page_pf = request.GET.get('page_pf', 1)
+    page_pj = request.GET.get('page_pj', 1)
+
+    paginator_pf = Paginator(clientespf_qs, 10)
+    paginator_pj = Paginator(clientespj_qs, 10)
+
+    clientespf = paginator_pf.get_page(page_pf)
+    clientespj = paginator_pj.get_page(page_pj)
+
+    context = {
+        'clientespf': clientespf,
+        'clientespj': clientespj,
+        'buscar': request.GET.get('buscar', ''),
+        'request': request,  # necessário para bootstrap_pagination url=request.get_full_path
+    }
+    return render(request, 'clientes.html', context)
+'''
+'''class ClientesView(TemplateView):
+    template_name = 'clientes.html'
+    paginate_by = 10
+    context_object_name = 'clientes'  # Use 'clientes' para ser mais claro no template
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Pega o termo de busca da URL
+        buscar = self.request.GET.get('buscar')
+
+        # Busca e filtra a lista de Clientes PF
+        clientes_pf = ClientePF.objects.all()
+        if buscar:
+            clientes_pf = clientes_pf.filter(nome__icontains=buscar)
+
+        # Busca e filtra a lista de Clientes PJ
+        clientes_pj = ClientePJ.objects.all()
+        if buscar:
+            # Lembre-se que em ClientePJ, o nome da empresa está no campo 'nome'
+            clientes_pj = clientes_pj.filter(nome__icontains=buscar)
+
+        # Adiciona as duas listas e o termo de busca ao contexto
+        context['clientes_pf'] = clientes_pf
+        context['clientes_pj'] = clientes_pj
+        context['buscar'] = buscar if buscar else ''
+
+        return context'''
--- veiculos/forms.py
+++ veiculos/forms.py
@@ -1,77 +1,45 @@
 from django import forms
-from clientes.models import Pessoa
+from django.contrib.contenttypes.models import ContentType
+
+from clientes.models import ClientePJ, ClientePF
 from veiculos.models import Veiculo
 
 
-# veiculos/forms.py
-
-from django import forms
-from .models import Veiculo
-from clientes.models import ClientePF, ClientePJ  # Importe seus modelos de cliente
-
 class VeiculoModelForm(forms.ModelForm):
-    # 1. Crie um campo de escolha que não está no modelo
-    # Este campo vai mostrar todos os clientes (PF e PJ) em uma lista.
-    cliente_choice = forms.ChoiceField(label='Cliente', required=True)
+    cliente = forms.ChoiceField(label='Cliente', required=True)
 
     class Meta:
         model = Veiculo
-        # 2. Exclua os campos da GFK da renderização automática do formulário
-        exclude = ('content_type', 'object_id', 'cliente')
+        fields = ['placa', 'marca', 'modelo', 'cor', 'qtd_rodas']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # 3. Preencha as opções do campo de escolha dinamicamente
-        clientes_pf = [
-            (f'clientepf_{c.pk}', f'{c.nome} (PF)') for c in ClientePF.objects.all()
-        ]
-        clientes_pj = [
-            (f'clientepj_{c.pk}', f'{c.empresa} (PJ)') for c in ClientePJ.objects.all()
-        ]
-        # Junta as duas listas
-        self.fields['cliente_choice'].choices = [('', '---------')] + clientes_pf + clientes_pj
+        clientes_pf = ClientePF.objects.all()
+        clientes_pj = ClientePJ.objects.all()
+        choices = [('', '---------')]
 
-# class VeiculoModelForm(forms.ModelForm):
-#     cliente = forms.ModelMultipleChoiceField(
-#         queryset=Pessoa.objects.all(),
-#         widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
-#         label="Clientes"
-#     )
-#
-#     class Meta:
-#         model = Veiculo
-#         fields = ['placa', 'marca', 'modelo', 'cor', 'qtd_rodas', 'cliente']
-#
+        for cliente in clientes_pf:
+            ct = ContentType.objects.get_for_model(cliente)
+            choices.append((f'{ct.id}-{cliente.id}', f'{cliente.nome} (PF)'))
 
+        for cliente in clientes_pj:
+            ct = ContentType.objects.get_for_model(cliente)
+            choices.append((f'{ct.id}-{cliente.id}', f'{cliente.nome} (PJ)'))
 
-    # def __init__(self, *args, **kwargs): buscar pj e pf e fazer a junção apra o input
-    #     super().__init__(*args, **kwargs)
+        self.fields['cliente'].choices = choices
 
-    #     clientes_pf = ClientePF.objects.all()
-    #     clientes_pj = ClientePJ.objects.all()
-    #     choices = [('', '---------')]
-    #
-    #     for cliente in clientes_pf:
-    #         ct = ContentType.objects.get_for_model(cliente)
-    #         choices.append((f'{ct.id}-{cliente.id}', f'{cliente.nome} (PF)'))
-    #
-    #     for cliente in clientes_pj:
-    #         ct = ContentType.objects.get_for_model(cliente)
-    #         choices.append((f'{ct.id}-{cliente.id}', f'{cliente.nome} (PJ)'))
-    #
-    #     self.fields['cliente'].choices = choices
+        # mantem o cliente na edição
+        if self.instance and self.instance.pk and self.instance.cliente:
+            ct = ContentType.objects.get_for_model(self.instance.cliente)
+            self.fields['cliente'].initial = f'{ct.id}-{self.instance.cliente.id}'
 
-        #     if self.instance and self.instance.pk and self.instance.cliente:
-    #         ct = ContentType.objects.get_for_model(self.instance.cliente)
-    #         self.fields['cliente'].initial = f'{ct.id}-{self.instance.cliente.id}'
-    #
-    # def save(self, commit=True):
-    #     cliente_data = self.cleaned_data['cliente'].split('-')
-    #     content_type_id = int(cliente_data[0])
-    #     object_id = int(cliente_data[1])
-    #
-    #     self.instance.content_type_id = content_type_id
-    #     self.instance.object_id = object_id
-    #
-    #     return super().save(commit)
+    def save(self, commit=True):
+        cliente_data = self.cleaned_data['cliente'].split('-')
+        content_type_id = int(cliente_data[0])
+        object_id = int(cliente_data[1])
+
+        self.instance.content_type_id = content_type_id
+        self.instance.object_id = object_id
+
+        return super().save(commit)
