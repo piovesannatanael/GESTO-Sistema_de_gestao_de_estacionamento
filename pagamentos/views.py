ad2e23f7efcabe1115df869113081a7d9756063b
Natanael <piovesannatanael@gmail.com>
2025-10-16 14:05:45 -0300

    CRUD pagamentos completo

--- pagamentos/views.py
+++ pagamentos/views.py
@@ -1,16 +1,18 @@
+# pagamento/views.py
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from pagamento.forms import PagamentoForm
-from pagamento.models import Pagamento
+from django.contrib import messages
+from .forms import PagamentoForm
+from .models import Pagamento
 
 
 class PagamentosView(ListView):
     model = Pagamento
     template_name = 'pagamentos.html'
     context_object_name = 'pagamentos'
-    paginate_by = 1
+    paginate_by = 10
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -18,10 +20,17 @@ class PagamentosView(ListView):
 
         if buscar:
             qs = qs.filter(
-                Q(data__icontains=buscar) |
-                Q(forma__icontains=buscar)
+                Q(estadia__id__icontains=buscar) |
+                Q(forma__icontains=buscar) |
+                Q(valor_total__icontains=buscar)
             )
         return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['buscar'] = self.request.GET.get('buscar', '')
+        return context
+
 
 class PagamentoCreateView(SuccessMessageMixin, CreateView):
     model = Pagamento
@@ -30,6 +39,11 @@ class PagamentoCreateView(SuccessMessageMixin, CreateView):
     success_url = reverse_lazy('pagamentos')
     success_message = 'Pagamento cadastrado com sucesso!'
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
+
+
 class PagamentoUpdateView(SuccessMessageMixin, UpdateView):
     model = Pagamento
     form_class = PagamentoForm
@@ -37,9 +51,13 @@ class PagamentoUpdateView(SuccessMessageMixin, UpdateView):
     success_url = reverse_lazy('pagamentos')
     success_message = 'Pagamento alterado com sucesso!'
 
+
 class PagamentoDeleteView(SuccessMessageMixin, DeleteView):
     model = Pagamento
     template_name = 'pagamento_apagar.html'
     success_url = reverse_lazy('pagamentos')
-    success_message = 'Pagamento excluido com sucesso!'
+    success_message = 'Pagamento excluído com sucesso!'
 
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
