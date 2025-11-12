aa06dced43a2292f5eb858a3ff4615af984c7c95
piovesannatanael <89926365+piovesannatanael@users.noreply.github.com>
2025-11-12 11:57:45 -0300

    Delete estadias directory

--- estadias/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class EstadaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'estadias'
-    verbose_name = 'Controle de Estadias'
