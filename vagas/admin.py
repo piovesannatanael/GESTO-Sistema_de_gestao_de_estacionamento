c5495cf0f5a0cf36d80a34e221ba778a97608374
piovesannatanael <89926365+piovesannatanael@users.noreply.github.com>
2025-11-12 11:58:56 -0300

    Delete vagas directory

--- vagas/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Vaga
-
-
-@admin.register(Vaga)
-class VagaAdmin(admin.ModelAdmin):
-    list_display = ('codigo', 'status')
-    list_filter = ('status',)
-    search_fields = ('codigo',)
