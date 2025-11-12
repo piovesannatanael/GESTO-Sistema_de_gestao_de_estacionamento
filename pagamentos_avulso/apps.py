d26842017361d9ac9d549901b0643bcdeaf401fd
piovesannatanael <89926365+piovesannatanael@users.noreply.github.com>
2025-11-12 11:58:38 -0300

    Delete pagamentos_avulso directory

--- pagamentos_avulso/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagamentoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pagamentos_avulso'
-    verbose_name = 'Controle de Pagamentos'
