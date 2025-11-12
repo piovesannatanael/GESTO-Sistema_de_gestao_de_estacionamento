3c5542e31666a84594f4e9673f9f7b9c2ed06837
piovesannatanael <89926365+piovesannatanael@users.noreply.github.com>
2025-11-12 11:58:50 -0300

    Delete pagamentos_modal directory

--- pagamentos_modal/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagamentosModalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pagamentos_modal'
-    verbose_name = 'Controle de Pagamentos por modalidade'
