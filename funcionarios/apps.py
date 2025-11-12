cb9b47b7f7799e4aacb2864f0ca1d50425b4f019
piovesannatanael <89926365+piovesannatanael@users.noreply.github.com>
2025-11-12 11:57:57 -0300

    Delete funcionarios directory

--- funcionarios/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class FuncionariosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'funcionarios'
-    verbose_name = 'Controle de Funcionarios'
