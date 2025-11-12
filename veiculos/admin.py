bb94c490c32183fed4914f7d43e954629979b942
piovesannatanael <89926365+piovesannatanael@users.noreply.github.com>
2025-11-12 11:56:49 -0300

    Delete veiculos directory

--- veiculos/admin.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-# from .models import Veiculo
-#
-#
-# @admin.register(Veiculo)
-# class VeiculoAdmin(admin.ModelAdmin):
-#     list_display = ('placa', 'marca', 'modelo', 'cor', 'categorias', 'plano',)
-#     list_filter = ('plano', 'categorias', 'marca',)
-#     search_fields = ('placa', 'marca', 'modelo',)
--- veiculos/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class VeiculosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'veiculos'
-    verbose_name = 'Controle de Veiculos'
